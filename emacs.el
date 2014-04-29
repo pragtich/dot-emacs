6cd051ac843035e429cab1f07f0c9f5e68bf0a91
unknown <jpg@PC1871.domein1.ap>
2014-04-29 15:17:52 +0200

    Added elpa, melpa; Included theming. Installed colortheme.

--- emacs.el
+++ emacs.el
@@ -1,3 +1,21 @@
+;; Add MELPA to packages sources
+;; Inspired by http://melpa.milkbox.net/#/getting-started
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;; On older emacsen also add the base library
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))  
+
+;; Initialize packages
+(package-initialize)
+
+;; show matching parens
+(show-paren-mode 1)
+
+;; take the short answer, y/n is yes/no
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 ;; Remove unused UI elements
 (tool-bar-mode 0)
 (menu-bar-mode 0)
@@ -7,8 +25,7 @@
 ;; shhht, give me some time to think, don't blink
 (blink-cursor-mode 0)
 
-;; show matching parens
-(show-paren-mode 1)
-
-;; take the short answer, y/n is yes/no
-(defalias 'yes-or-no-p 'y-or-n-p)
+;;    (require 'color-theme)
+;;    (color-theme-initialize)
+;;    (color-theme-kingsajz)
+(load-theme 'zenburn t)
