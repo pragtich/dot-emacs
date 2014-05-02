af20c1bc70a56468c3785fdab8e28f385834f450
Joris Pragt <pragtich@gmail.com>
2014-05-02 15:38:17 +0200

    Changed maximize feature to be more robust.

--- emacs.el
+++ emacs.el
@@ -32,8 +32,8 @@
 
 (setq sentence-end-double-space nil)
 
-(setq split-height-threshold 40)
-(setq split-width-threshold 80)
+(setq split-height-threshold 60)
+(setq split-width-threshold 90)
 
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
@@ -52,6 +52,40 @@
        (ido-completing-read
         "M-x "
         (all-completions "" obarray 'commandp))))))
+
+(defun delete-current-buffer-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
+
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
+
+(global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
 
 (global-set-key (kbd "M-j")
 (lambda ()
@@ -88,3 +122,7 @@
       '(kill-ring
         search-ring
         regexp-search-ring))
+
+; (if (eq system-type 'windows-nt)
+;   (add-hook 'after-init-hook '(lambda () (w32-send-sys-command #xf030))))
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
