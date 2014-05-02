c6bf395eb14ab60c22377dc1624ee552111d9040
unknown <jpg@PC1871.domein1.ap>
2014-05-02 10:36:43 +0200

    Added several internet finds to emacs.org

--- emacs.el
+++ emacs.el
@@ -16,6 +16,10 @@
 ;; take the short answer, y/n is yes/no
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+(setq sentence-end-double-space nil)
+
 ;; Remove unused UI elements
 (tool-bar-mode 0)
 (menu-bar-mode 0)
@@ -29,3 +33,20 @@
 ;;    (color-theme-initialize)
 ;;    (color-theme-kingsajz)
 (load-theme 'zenburn t)
+
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list" t)))
+
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
