ae3e02595ceb9827894e09137e7641b05e269066
Joris Pragt <pragtich@gmail.com>
2014-05-02 15:06:57 +0200

    Fixed a typo othat broke babel tangle stuff

--- emacs.el
+++ emacs.el
@@ -1,3 +1,5 @@
+(add-to-list 'org-structure-template-alist (list "S" "#+BEGIN_SRC emacs-lisp?\n\n#+END_SRC" "<src lang=\"?\">\n\n</src>" ))
+
 ;; Add MELPA to packages sources
 ;; Inspired by http://melpa.milkbox.net/#/getting-started
 (require 'package)
@@ -9,6 +11,9 @@
 
 ;; Initialize packages
 (package-initialize)
+
+(global-set-key (kbd "C-x g") 'magit-status) 
+(global-set-key (kbd "<f12>") 'magit-status)
 
 (setenv "GIT_ASKPASS" "git-gui--askpass")
 
@@ -26,6 +31,32 @@
 (global-set-key (kbd "RET") 'newline-and-indent)
 
 (setq sentence-end-double-space nil)
+
+(setq split-height-threshold 40)
+(setq split-width-threshold 80)
+
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(setq ido-create-new-buffer 'always)
+
+(setq ido-ignore-extensions t)
+
+(global-set-key
+   "\M-x"
+   (lambda ()
+     (interactive)
+     (call-interactively
+      (intern
+       (ido-completing-read
+        "M-x "
+        (all-completions "" obarray 'commandp))))))
+
+(global-set-key (kbd "M-j")
+(lambda ()
+(interactive)
+(join-line -1)))
 
 ;; Remove unused UI elements
 (tool-bar-mode 0)
@@ -57,3 +88,28 @@
       '(kill-ring
         search-ring
         regexp-search-ring))
+
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
