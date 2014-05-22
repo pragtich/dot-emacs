58196f181866d1da249ba2f3bd33a9e83a33f80d
Joris Pragt <pragtich@gmail.com>
2014-05-22 16:12:58 +0200

    Added profiling to init.el

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; Profiling of emacs init process (see https://github.com/dholm/benchmark-init-el)
+(add-to-list 'load-path "~/.emacs.d/elpa/benchmark-init-20140510.732/")
+(require 'benchmark-init)
+
 
 ;; Test: proves that this init file is being found 
 ;(tool-bar-mode -1)
