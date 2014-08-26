6feb64a746316ba8fa15be862aee3a6c8d8016a0
Joris Pragt <pragtich@gmail.com>
2014-08-26 12:06:31 +0200

    Restarted emacs: new emacs.el

--- emacs.el
+++ emacs.el
@@ -16,6 +16,12 @@
 
 (ido-vertical-mode 1)
 
+(add-to-list 'load-path "~/.emacs.d/lisp/python-mode")
+
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
 (global-set-key (kbd "C-x g") 'magit-status) 
 (global-set-key (kbd "<f12>") 'magit-status)
 
