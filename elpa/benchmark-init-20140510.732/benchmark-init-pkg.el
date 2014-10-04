a28043c58a2ba233e87c309cb0f9e28a2270bcf1
Joris Pragt <pragtich@github>
2014-10-04 13:03:42 +0200

    Manually reinstalled elpa packages: need to automate this

--- elpa/benchmark-init-20140510.732/benchmark-init-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "benchmark-init" "20140510.732" "Benchmarks Emacs require and load calls" 'nil :keywords ("benchmark"))
--- elpa/zenburn-theme-20140418.13/zenburn-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "zenburn-theme" "20140418.13" "A low contrast color theme for Emacs." 'nil :url "http://github.com/bbatsov/zenburn-emacs")
