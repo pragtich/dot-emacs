6aed9034696115d08b587594a040bc2eb25af0c8
Joris Pragt <pragtich@github>
2014-11-10 16:16:49 +0100

    Compiled emacs.el

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 ;; Profiling of emacs init process (see https://github.com/dholm/benchmark-init-el)
-(add-to-list 'load-path "~/.emacs.d/el-get/benchmark-init/")
-(require 'benchmark-init)
-(let ((benchmark-init.el "~/.emacs.d/el-get/benchmark-init/benchmark-init.el"))
-  (when (file-exists-p benchmark-init.el)
-    (load benchmark-init.el)))
+;(add-to-list 'load-path "~/.emacs.d/el-get/benchmark-init/")
+;(require 'benchmark-init)
+;(let ((benchmark-init.el "~/.emacs.d/el-get/benchmark-init/benchmark-init.el"))
+;  (when (file-exists-p benchmark-init.el)
+;    (load benchmark-init.el)))
 
 ;; Test: proves that this init file is being found 
 ;(tool-bar-mode -1)
