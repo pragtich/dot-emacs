9db791c01e89aab16c1f1478e833b691fd52efb3
Joris Pragt <pragtich@gmail.com>
2014-12-16 16:02:06 +0100

    Implemented endless parentheses' prefix to kill entire line trick.
    http://endlessparentheses.com/kill-entire-line-with-prefix-argument.html?source=rss

--- emacs.el
+++ emacs.el
@@ -211,6 +211,25 @@
   ;; shhht, give me some time to think, don't blink
   (blink-cursor-mode 0)
 
+(defmacro bol-with-prefix (function)
+  "Define a new function which calls FUNCTION.
+Except it moves to beginning of line before calling FUNCTION when
+called with a prefix argument. The FUNCTION still receives the
+prefix argument."
+  (let ((name (intern (format "endless/%s-BOL" function))))
+    `(progn
+       (defun ,name (p)
+         ,(format 
+           "Call `%s', but move to BOL when called with a prefix argument."
+           function)
+         (interactive "P")
+         (when p
+           (forward-line 0))
+         (call-interactively ',function))
+       ',name)))
+(global-set-key [remap org-kill-line] (bol-with-prefix org-kill-line))
+(global-set-key [remap kill-line] (bol-with-prefix kill-line))
+
 ;;    (require 'color-theme)
 ;;    (color-theme-initialize)
 ;;    (color-theme-kingsajz)
