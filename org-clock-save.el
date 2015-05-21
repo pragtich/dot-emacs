d6771f914e877deb88369cd2696c13e09dd6b102
Joris Pragt <pragtich@gmail.com>
2015-05-21 16:08:17 +0200

    Added org-clock-save to repo

---
+++ org-clock-save.el
@@ -0,0 +1,2 @@
+;; org-persist.el - PC2644 at <2015-05-21 do 12:58>
+(setq stored-clock-history '(("c:/Users/JPG/Dropbox/org/jpg.org" . 7082) ("c:/Users/JPG/Dropbox/org/jpg.org" . 7060) ("c:/Users/JPG/Dropbox/org/jpg.org" . 193) ("c:/Users/JPG/Dropbox/org/jpg.org" . 7168)))
