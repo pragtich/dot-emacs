a8b9ce13786c1fe0c5c8682b80afa267057ccc6f
Joris Pragt <pragtich@github>
2015-09-05 17:41:23 +0200

    remove custom.el bookmark file thing

--- custom.el
+++ custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "c:\\Users\\JPG\\AppData\\Roaming\\.emacs.d\\bookmarks")
  '(custom-safe-themes
    (quote
     ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))))
