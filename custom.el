2a4bfd6f0ef8743e6efb20a30c0facded7c34c98
Joris Pragt <pragtich@github>
2017-01-09 18:55:33 +0100

    Added company-irony

--- custom.el
+++ custom.el
@@ -6,6 +6,10 @@
  '(custom-safe-themes
    (quote
     ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(nil nil t)
+ '(package-selected-packages
+   (quote
+    (company-irony which-key undo-tree transpose-frame swiper-helm platformio-mode markdown-mode magit key-chord ivy-hydra headlong guide-key git-gutter-fringe expand-region exec-path-from-shell diminish counsel company color-theme-sanityinc-solarized bookmark+ benchmark-init beacon ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,4 +17,3 @@
  ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "blue1"))))
  '(helm-source-header ((t (:inherit source :foreground "#b58900" :weight bold :height 1)))))
-)
